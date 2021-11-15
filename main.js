dece40afd1e3cb78877724a37d0e08faa0231463
hugmouse <mysh@tuta.io>
2021-11-15 20:08:32 +0800

    [L6] Add some event handlers
    And some methods too!

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const app = Vue.createApp({
     data() {
         return {
+            cart: 0,
             product: 'Socks',
             description: 'Some description I guess',
             imageURL: './assets/images/socks_green.jpg',
@@ -10,10 +11,21 @@ const app = Vue.createApp({
             onSale: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                { id: 2234, color: 'green' },
-                { id: 2235, color: 'blue' }
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' }
             ],
             sizes: ['big', 'not so big', 'smol']
+        }
+    },
+    methods: {
+        addToCart() {
+            if(this.inventory !== this.cart) { this.cart += 1 }
+        },
+        removeFromCart() {
+            if(this.cart > 0) { this.cart -= 1 }
+        },
+        updateImage(variantImage) {
+            this.imageURL = variantImage
         }
     }
 })
