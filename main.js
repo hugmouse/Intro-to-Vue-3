267ce0afc5c380dc1123fd59da034bd003a379c2
hugmouse <mysh@tuta.io>
2021-11-15 17:18:38 +0800

    [L3] Add dynamic image src and alt tag

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            description: 'Some description I guess'
+            description: 'Some description I guess',
+            imageURL: './assets/images/socks_green.jpg',
+            imageDescription: 'Socks image'
         }
     }
 })
