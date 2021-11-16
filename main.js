600ab0170b5043f08e748a34564c7a3d78767496
hugmouse <mysh@tuta.io>
2021-11-16 11:55:45 +0800

    [L8] Use computed properties for title, image and stock

--- main.js
+++ main.js
@@ -3,16 +3,16 @@ const app = Vue.createApp({
         return {
             cart: 0,
             product: 'Socks',
+            brand: 'Vue Mastery',
             description: 'Some really handy socks!',
-            imageURL: './assets/images/socks_green.jpg',
+            selectedVariant: 0,
             imageDescription: 'Socks image',
-            inStock: true,
             inventory: 8,
             onSale: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' }
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 }
             ],
             sizes: ['big', 'not so big', 'smol']
         }
@@ -24,8 +24,22 @@ const app = Vue.createApp({
         removeFromCart() {
             if(this.cart > 0) { this.cart -= 1 }
         },
-        updateImage(variantImage) {
-            this.imageURL = variantImage
+        updateVariant(index) {
+            this.selectedVariant = index
+        }
+    },
+    computed: {
+        title() {
+            if(this.onSale) {
+                return this.brand + ' ' + this.product + ' is on sale'
+            }
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity
         }
     }
 })
